05f2f8446557b512e427c463166561db9c91858a
CadeM12 McNelly <cadem2@educbe.ca>
2025-04-16 00:32:02 +0000

    pathfinding working

--- moveLogic.js
+++ moveLogic.js
@@ -1,42 +1,10 @@
 let gameStateG;
 let board = {};
-let moveSafety;
 
 export default function move(gameState){
     gameStateG = gameState;
 
-    moveSafety = {
-        up: true,
-        down: true,
-        left: true,
-        right: true
-    };
-    
-    // We've included code to prevent your Battlesnake from moving backwards
     const myHead = gameState.you.body[0];
-    const myNeck = gameState.you.body[1];
-    
-    if (myNeck.x < myHead.x || myHead.x == 0) {        // Neck is left of head, don't move left
-        moveSafety.left = false;
-        
-    } 
-    if (myNeck.x > myHead.x || myHead.x == gameState.board.width-1) { // Neck is right of head, don't move right
-        moveSafety.right = false;
-        
-    } 
-    if (myNeck.y < myHead.y || myHead.y == 0) { // Neck is below head, don't move down
-        moveSafety.down = false;
-        
-    } 
-    if (myNeck.y > myHead.y || myHead.y == gameState.board.height-1) { // Neck is above head, don't move up
-        moveSafety.up = false;
-    }
-    
-    // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    // gameState.board contains an object representing the game board including its width and height
-    // https://docs.battlesnake.com/api/objects/board
-
-    //console.log(gameState.board);
 
     let c = 0;
     for(let i=0; i<gameState.board.height; i++){
@@ -48,40 +16,24 @@ export default function move(gameState){
     
     connectNodes();
 
-    let path = aStar(board, getNodeId(myHead), getNodeId(gameState.board.food[0])).path[1] ?? board[getNodeId(myHead)].connections[Math.floor(Math.random(board[getNodeId(myHead)].connections.length))][0]
+    let state;
+    let pathfindTo;
 
-    //console.log(aStar(board, getNodeId(myHead), getNodeId(gameState.board.food[0])).path[0])
-    //console.log(board[getNodeId(myHead)].connections[Math.floor(Math.random(board[getNodeId(myHead)].connections.length))][0])
-    //console.log(path);
-    let nextMove = calculateNextMove(path);
-
-    // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    // gameState.you contains an object representing your snake, including its coordinates
-    // https://docs.battlesnake.com/api/objects/battlesnake
-    
-    
-    // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    // gameState.board.snakes contains an array of enemy snake objects, which includes their coordinates
-    // https://docs.battlesnake.com/api/objects/battlesnake
-    
-    // Are there any safe moves left?
-    
-    //Object.keys(moveSafety) returns ["up", "down", "left", "right"]
-    //.filter() filters the array based on the function provided as an argument (using arrow function syntax here)
-    //In this case we want to filter out any of these directions for which moveSafety[direction] == false
-    const safeMoves = Object.keys(moveSafety).filter(direction => moveSafety[direction]);
-    if (safeMoves.length == 0) {
-        console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
-        return { move: "down" };
+    if(gameState.you.health < 50 || gameState.you.body.length < 7){
+        state = 'hungry';
+        pathfindTo = nearestFood();
+    } else if(gameState.board.snakes.length > 1){
+        state = 'hunting';
+        pathfindTo = snakeHead(nearestSnakeHead());
+    } else {
+        state = 'chilling';
+        pathfindTo = board[getNodeId(myHead)].connections[Math.floor(Math.random()*board[getNodeId(myHead)].connections.length)][0];
+        console.log(pathfindTo);
     }
-    
-    // Choose a random move from the safe moves
-    //const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
-    //console.log([board, getNodeId(myHead), getNodeId(gameState.board.food[0])]);
-    //console.log(aStar(board, getNodeId(myHead), getNodeId(gameState.board.food[0])));
 
-    // TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    // gameState.board.food contains an array of food coordinates https://docs.battlesnake.com/api/objects/board
+    let path = aStar(board, getNodeId(myHead), pathfindTo).path[1];
+
+    let nextMove = calculateNextMove(path);
     
     console.log(`MOVE ${gameState.turn}: ${nextMove}`)
     return { move: nextMove };
@@ -101,7 +53,11 @@ function connectNodes(){
     for(let i = 0; i < (gameStateG.board.width * gameStateG.board.height); i++){
         for(let j = 0; j < (gameStateG.board.width * gameStateG.board.height); j++)
         {
-            if(((board[j].position.x == board[i].position.x-1 && board[j].position.y == board[i].position.y && moveSafety.left) || (board[j].position.x == board[i].position.x+1 && board[j].position.y == board[i].position.y && moveSafety.right) || (board[j].position.y == board[i].position.y-1 && board[j].position.x == board[i].position.x && moveSafety.up) || (board[j].position.y == board[i].position.y+1 && board[j].position.x == board[i].position.x && moveSafety.down))&&(!snakeBodies.includes(j))){
+            if(((board[j].position.x == board[i].position.x-1 && board[j].position.y == board[i].position.y) || 
+            (board[j].position.x == board[i].position.x+1 && board[j].position.y == board[i].position.y) || 
+            (board[j].position.y == board[i].position.y-1 && board[j].position.x == board[i].position.x) || 
+            (board[j].position.y == board[i].position.y+1 && board[j].position.x == board[i].position.x)) &&
+            (!snakeBodies.includes(j))){
                 board[i].connections.push([j, 1]);
             };
         }
@@ -120,9 +76,6 @@ function aStar(graph, start, target){
         if (current.node === target) {
             return { path: current.path, cost: gScores[target] };
         };
-        //console.log(current);
-        //console.log(current.node)
-        //console.log(graph[current.node].connections);
         
         for (let [neighbor, cost] of graph[current.node].connections) {
             let tentativeG = gScores[current.node] + cost;
@@ -136,7 +89,6 @@ function aStar(graph, start, target){
         };
     };
     
-    // If no path is found
     return { path: [], cost: Infinity };
 };
 
@@ -150,6 +102,33 @@ function heuristic(start, target, nodes){
 
 function getNodeId(pos){
     return pos.y*gameStateG.board.width + pos.x;
+}
+
+function nearestFood(){
+    let nearest = {path: Infinity, food: undefined};
+    for(let i = 0; i < gameStateG.board.food.length; i++){
+        let dist = Math.sqrt((gameStateG.board.food[i].x - gameStateG.you.body[0].x)**2 + (gameStateG.board.food[i].y - gameStateG.you.body[0].y)**2)
+        if(dist < nearest.path){
+            nearest.path = dist;
+            nearest.food = i;
+        }
+    }
+    return getNodeId(gameStateG.board.food[nearest.food]);
+}
+
+function nearestSnakeHead(){
+    let nearest = {path: Infinity, snake: undefined};
+    for(let i = 0; i < gameStateG.board.snakes.length; i++){
+        if(Math.sqrt(gameStateG.board.snakes[i].body[0].x**2 + gameStateG.board.snakes[i].body[0].y**2) < nearest.path){
+            nearest.snake = i;
+        }
+    }
+
+    return gameStateG.board.snakes[nearest.snake].id;
+}
+
+function snakeHead(){
+
 }
 
 function calculateNextMove(path){
